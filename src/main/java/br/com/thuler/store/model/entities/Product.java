f74b8ca8905856af8813eef5d48c5ecc4dd3f49e
Henry Thuler <henrythuler2003@outlook.com>
2024-05-16 15:53:05 -0300

    feat(product): Relationship between Product and OrderItem

--- src/main/java/br/com/thuler/store/model/entities/Product.java
+++ src/main/java/br/com/thuler/store/model/entities/Product.java
@@ -1,5 +1,6 @@
 package br.com.thuler.store.model.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.util.HashSet;
@@ -21,6 +22,9 @@ public class Product {
     @ManyToMany
     @JoinTable(name = "T_SPRING_PRODUCT_CATEGORY", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
     private Set<Category> categories = new HashSet<>();
+
+    @OneToMany(mappedBy = "id.product")
+    private Set<OrderItem> items;
 
     public Product() {}
 
@@ -74,6 +78,13 @@ public class Product {
 
     public Set<Category> getCategories() {
         return categories;
+    }
+
+    @JsonIgnore
+    public Set<Order> getOrders(){
+        Set<Order> orders = new HashSet<>();
+        items.forEach(i -> orders.add(i.getOrder()));
+        return orders;
     }
 
     @Override
